b32780287b2382e119b28589d3b8a7898f7b0782
Alexandru Q <156797918+qlexqndru@users.noreply.github.com>
2024-12-24 11:56:22 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,31 +1,24 @@
 #!/usr/bin/env -S nix shell nixpkgs#jq -c bash
-
 set -euo pipefail
-
 regex="^[0-9]\.[0-9]\.[0-9].*$"
-
 info="info.json"
 oldversion=$(jq -rc '.version' "$info")
-
 url="https://api.github.com/repos/zen-browser/desktop/releases?per_page=1"
 version="$(curl -s "$url" | jq -rc '.[0].tag_name')"
 
 if [ "$oldversion" != "$version" ] && [[ "$version" =~ $regex ]]; then
   echo "Found new version $version"
   sharedUrl="https://github.com/zen-browser/desktop/releases/download"
-
-  genericUrl="${sharedUrl}/${version}/zen.linux-generic.tar.bz2"
-  specificUrl="${sharedUrl}/${version}/zen.linux-specific.tar.bz2"
-
-  # perform downloads in parallel
-  echo "Prefetching files..."
-  nix store prefetch-file "$genericUrl" --log-format raw --json | jq -rc '.hash' >/tmp/genericHash &
-  nix store prefetch-file "$specificUrl" --log-format raw --json | jq -rc '.hash' >/tmp/specificHash &
-  wait
-  genericHash=$(</tmp/genericHash)
-  specificHash=$(</tmp/specificHash)
-
-  echo '{"version":"'"$version"'","generic":{"hash":"'"$genericHash"'","url":"'"$genericUrl"'"},"specific":{"hash":"'"$specificHash"'","url":"'"$specificUrl"'"}}' >"$info"
+  x86_64Url="${sharedUrl}/${version}/zen.linux-x86_64.tar.bz2"
+  
+  echo "Prefetching file..."
+  x86_64Hash=$(nix store prefetch-file "$x86_64Url" --log-format raw --json | jq -rc '.hash')
+  
+  echo '{
+    "version":"'"$version"'",
+    "hash":"'"$x86_64Hash"'",
+    "url":"'"$x86_64Url"'"
+  }' | jq -c . >"$info"
 else
   echo "zen is up to date"
 fi
