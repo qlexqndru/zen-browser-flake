5af6e3144a518d95dc5a33b00e00a3227694161b
Alexandru Q <156797918+qlexqndru@users.noreply.github.com>
2024-12-24 11:59:38 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,34 +1,28 @@
 {
   description = "Zen Browser";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
-
   outputs = {
     self,
     nixpkgs,
   }: let
     system = "x86_64-linux";
-
     info = builtins.fromJSON (builtins.readFile ./info.json);
-
     pkgs = nixpkgs.legacyPackages.${system};
-
-    mkZen = sourceInfo: pkgs.callPackage ./package.nix {inherit sourceInfo;};
+    mkZen = pkgs.callPackage ./package.nix {
+      sourceInfo = {
+        variant = "x86_64";
+        src = {
+          url = info.url;
+          hash = info.hash;
+        };
+        inherit (info) version;
+      };
+    };
   in {
     packages."${system}" = {
-      generic = mkZen {
-        variant = "generic";
-        src = info.generic;
-        inherit (info) version;
-      };
-      specific = mkZen {
-        variant = "specific";
-        src = info.specific;
-        inherit (info) version;
-      };
-      default = self.packages.${system}.specific;
+      default = mkZen;
     };
   };
 }
